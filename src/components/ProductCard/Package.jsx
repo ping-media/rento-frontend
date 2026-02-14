dee37e102889fab9182d8f033aa15d517382b674
kakkar2 <lalitkakkar50@gmail.com>
2026-02-14 18:01:41 +0530

    misc: change the size of vehicle image in packages component

--- src/components/ProductCard/Package.jsx
+++ src/components/ProductCard/Package.jsx
@@ -57,17 +57,20 @@ const Package = () => {
           addDaysToDateForRide(pkg.planDuration, formatDate(baseDate)),
           currentTime,
         );
+        const isPackageTextInclude = pkg.planName
+          ?.toLowerCase()
+          .includes("package");
 
         return (
           <SwiperSlide key={pkg._id} className="mb-4">
             <Link
               to={`/search/${selectedStation?.stationId}?BookingStartDateAndTime=${pickupDateAndTime}&BookingEndDateAndTime=${endDate}&vehiclePlan=${pkg._id}`}
             >
-              <div className="relative rounded-md overflow-hidden shadow-md bg-white px-4 py-2">
+              <div className="relative rounded-md overflow-hidden shadow-md bg-white px-3.5 2xl:px-4 py-2">
                 <div className="absolute inset-0 bg-[radial-gradient(circle,_#94a3b8_0.9px,_transparent_0.9px)] bg-[length:8px_8px] opacity-30 pointer-events-none" />
 
                 <div className="flex items-center relative z-10">
-                  <div className="w-20 h-20">
+                  <div className="w-36 h-36 2xl:w-40 2xl:h-40">
                     <img
                       src={images[index % images.length]}
                       loading="lazy"
@@ -77,10 +80,12 @@ const Package = () => {
                   </div>
 
                   <div className="flex-1 p-3">
-                    <h2 className="text-lg font-extrabold line-clamp-1 uppercase text-gray-700">
+                    <h2
+                      className="text-lg font-extrabold line-clamp-1 uppercase text-gray-700"
+                      title={pkg.planName}
+                    >
                       {pkg.planName}
-                      {!pkg.planName?.toLowerCase().includes("package") &&
-                        " Package"}
+                      {!isPackageTextInclude && " Package"}
                     </h2>
 
                     <p className="text-sm mt-3">
